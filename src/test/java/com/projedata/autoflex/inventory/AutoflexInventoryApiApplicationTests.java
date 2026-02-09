24d882e1956b3e4f6d2307bc0ad92222cbe443b8
Winiicius <winiciusalexandre@gmail.com>
2026-02-08 23:44:37 -0300

    chore: initialize Java project

---
+++ src/test/java/com/projedata/autoflex/inventory/AutoflexInventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projedata.autoflex.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoflexInventoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
