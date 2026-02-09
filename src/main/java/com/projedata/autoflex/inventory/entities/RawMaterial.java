4400025f2bca47b7f1d702da3bb8ae5c69f88bb1
Winiicius <winiciusalexandre@gmail.com>
2026-02-09 17:01:24 -0300

    feat: add RawMaterial entity with Unit enum

---
+++ src/main/java/com/projedata/autoflex/inventory/entities/RawMaterial.java
@@ -0,0 +1,33 @@
+package com.projedata.autoflex.inventory.entities;
+
+import com.projedata.autoflex.inventory.entities.enums.Unit;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "raw_materials")
+public class RawMaterial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Unit unit;
+
+    @Column(nullable = false)
+    private float stockQuantity;
+}
