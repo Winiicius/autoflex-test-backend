a4aab5abc28293c6f0dd65638e2113444a32cbb2
Winiicius <winiciusalexandre@gmail.com>
2026-02-10 19:34:58 -0300

    feat: add base exceptions

---
+++ src/main/java/com/projedata/autoflex/inventory/exceptions/ConflictException.java
@@ -0,0 +1,7 @@
+package com.projedata.autoflex.inventory.exceptions;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
