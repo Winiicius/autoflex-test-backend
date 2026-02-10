934a2251df88176c66429eb7addf6c76c88effd9
Winiicius <winiciusalexandre@gmail.com>
2026-02-09 22:29:41 -0300

    feat: add User repository

---
+++ src/main/java/com/projedata/autoflex/inventory/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.projedata.autoflex.inventory.repositories;
+
+import com.projedata.autoflex.inventory.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
