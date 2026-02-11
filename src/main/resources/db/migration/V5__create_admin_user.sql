d1c7c7e7d643f4adb81d766e230ddc16ba6ab362
Winiicius <winiciusalexandre@gmail.com>
2026-02-11 18:07:16 -0300

    feat: add migration to insert default admin user

---
+++ src/main/resources/db/migration/V5__create_admin_user.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (name, email, password, role, active)
+VALUES (
+  'Autoflex Admin',
+  'admin@autoflex.com',
+  '$2a$12$UaSSMZBlAF4YLWY8fxkWp.x41C77sJ/BBlTeORTfSI.CV30SV4thG', -- senha: autoflex
+  'ADMIN',
+  true
+);
