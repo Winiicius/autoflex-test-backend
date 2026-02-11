add26dbab8132a28799f07e4e80aadb03596c0c5
Winiicius <winiciusalexandre@gmail.com>
2026-02-11 10:52:41 -0300

    feat: add Product service

---
+++ src/main/java/com/projedata/autoflex/inventory/services/ProductService.java
@@ -0,0 +1,19 @@
+package com.projedata.autoflex.inventory.services;
+
+import com.projedata.autoflex.inventory.dtos.product.ProductRequest;
+import com.projedata.autoflex.inventory.dtos.product.ProductResponse;
+
+import java.util.List;
+
+public interface ProductService {
+
+    ProductResponse create(ProductRequest request);
+
+    List<ProductResponse> findAll();
+
+    ProductResponse findById(Long id);
+
+    ProductResponse update(Long id, ProductRequest request);
+
+    void delete(Long id);
+}
